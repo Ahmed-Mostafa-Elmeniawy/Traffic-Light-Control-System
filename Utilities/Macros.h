a06a7653c086d19b6297edccaae4cfe0942c04bd
Ahmed Mostafa Elmeniawy <128949419+Ahmed-Mostafa-Elmeniawy@users.noreply.github.com>
2023-04-16 01:20:13 +0200

    Delete Utilities directory

--- Utilities/Macros.h
+++
@@ -1,9 +0,0 @@
-#ifndef MACROS_H_
-#define MACROS_H_
-
-#define CLR_BIT(REG,BIT)  REG &= ~(1<<BIT)
-#define SET_BIT(REG,BIT)  REG |=  (1<<BIT)
-#define TOG_BIT(REG,BIT)  REG ^=  (1<<BIT)
-#define GET_BIT(REG,BIT)  ((REG>>BIT) & 1)
-
-#endif
